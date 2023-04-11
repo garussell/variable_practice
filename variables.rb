3bc8dbc6418981adf9896acc8c9cf418865928c3
Allen Russell <garyrusselljr@gmail.com>
2023-04-11 14:12:27 -0600

    Add boolean variables and puts statements

--- variables.rb
+++ variables.rb
@@ -3,3 +3,14 @@ animals = "Pongo and Lola"
 hobbies = "Guitar, drums, and coding."
 weight = 167.5
 height = 65.5
+married = true
+turing_2305 = true
+
+name = "Allen Russell"
+animals = "Lola and Pongo"
+hobbies = "coding and stretching legs."
+
+puts "His name is #{name}."
+puts "His animals are #{animals}."
+puts "His weight is #{weight}lbs and height is #{height}in."
+puts "He enjoys #{hobbies}."
