d3d0d3638f0e83e0923a813969e84b1bcd97980f
Allen Russell <garyrusselljr@gmail.com>
2023-04-14 13:32:23 -0600

    Add variables per feedback

--- variables.rb
+++ variables.rb
@@ -3,6 +3,8 @@ animals = "Pongo and Lola"
 hobbies = "Guitar, drums, and coding."
 weight = 167.5
 height = 65.5
+age = 36
+birth_year = 1986
 married = true
 turing_2305 = true
 
@@ -12,5 +14,6 @@ hobbies = "coding and stretching legs."
 
 puts "His name is #{name}."
 puts "His animals are #{animals}."
+puts "Born in #{birth_year}, he is #{age} years old."
 puts "His weight is #{weight}lbs and height is #{height}in."
 puts "He enjoys #{hobbies}."
